46344344f7c8f642950d41f22aa1b8c85c7e70b6
Furkan Çöl <furkan.col@ogr.ksbu.edu.tr>
2025-08-01 17:28:31 +0300

    Add_Room_modal

---
+++ src/Altinay.Application.Contracts/Meeting/RoomInputListDto.cs
@@ -0,0 +1,15 @@
+﻿using Altinay.Enums;
+using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Altinay.Meeting
+{
+    public class RoomInputListDto : PagedAndSortedResultRequestDto
+    {
+        public string? Filter { get; set; }
+        public Guid? RoomID { get; set; }
+        public Guid? FloorID { get; set; }
+        public int? Capacity { get; set; }
+        public Availibility availibility { get; set; }
+    }
+}
